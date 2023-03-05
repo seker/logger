9fd02dd1c66baa6d5223b139d11a65cf4bbe3a05
xinjian <04070628@163.com>
2023-03-05 15:42:23 +0800

    添加android demo

---
+++ android/app/src/main/kotlin/seker/logger/android/Application.kt
@@ -0,0 +1,12 @@
+package seker.logger.android
+
+/**
+ *
+ * @author xinjian
+ */
+class Application : android.app.Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+    }
+}
