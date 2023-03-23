e7eb8b22acbe8d1c77ee5bfd7384e1b0440a39d3
xinjian <04070628@163.com>
2023-03-23 17:28:58 +0800

    对外接口对Java语言实现重载

--- android/app/src/main/kotlin/seker/logger/android/ui/main/MainViewModel.kt
+++ android/app/src/main/kotlin/seker/logger/android/ui/main/MainViewModel.kt
@@ -11,6 +11,46 @@ class MainViewModel(application: Application) : AndroidViewModel(application) {
     init {
         AsyncTaskExecutor.getInstance().scheduleAtFixedRate({
             Log.i("mainVM", "time:${System.currentTimeMillis()}, application=$application")
+
+            Log.v("v : msg")
+            Log.v(RuntimeException("v : tr"))
+            Log.v(TAG, "v : msg")
+            Log.v(TAG, "v : msg", RuntimeException("v : tr"))
+            Log.v(TAG, RuntimeException("v : tr"))
+
+            Log.d("d : msg")
+            Log.d(RuntimeException("d : tr"))
+            Log.d(TAG, "d : msg")
+            Log.d(TAG, "d : msg", RuntimeException("d : tr"))
+            Log.d(TAG, RuntimeException("d : tr"))
+
+            Log.i("i : msg")
+            Log.i(RuntimeException("i : tr"))
+            Log.i(TAG, "i : msg")
+            Log.i(TAG, "i : msg", RuntimeException("i : tr"))
+            Log.i(TAG, RuntimeException("i : tr"))
+
+            Log.w("w : msg")
+            Log.w(RuntimeException("w : tr"))
+            Log.w(TAG, "w : msg")
+            Log.w(TAG, "w : msg", RuntimeException("w : tr"))
+            Log.w(TAG, RuntimeException("w : tr"))
+
+            Log.e("e : msg")
+            Log.e(RuntimeException("e : tr"))
+            Log.e(TAG, "e : msg")
+            Log.e(TAG, "e : msg", RuntimeException("e : tr"))
+            Log.e(TAG, RuntimeException("e : tr"))
+
+            Log.f("f : msg")
+            Log.f(RuntimeException("f : tr"))
+            Log.f(TAG, "f : msg")
+            Log.f(TAG, "f : msg", RuntimeException("f : tr"))
+            Log.f(TAG, RuntimeException("f : tr"))
         }, "mainVM", 1, 5, TimeUnit.SECONDS)
+    }
+
+    companion object {
+        private const val TAG = "MainViewModel"
     }
 }
--- android/logger/src/main/kotlin/seker/logger/Log.kt
+++ android/logger/src/main/kotlin/seker/logger/Log.kt
@@ -33,7 +33,7 @@ object Log {
         LogJni.setMinutes(minutes)
     }
 
-    fun log(priority: Int, tag: String = TAG, msg: String? = null, tr: Throwable? = null) : Int {
+    fun log(priority: Int, tag: String, msg: String? = null, tr: Throwable? = null) : Int {
         return if (null == msg)  {
             if (null == tr) {
                 -1
@@ -49,12 +49,44 @@ object Log {
         }
     }
 
+    fun v(msg: String): Int {
+        return v(TAG, msg, null)
+    }
+
+    fun v(tr: Throwable): Int {
+        return v(TAG, null, tr)
+    }
+
+    fun v(tag: String = TAG, msg: String): Int {
+        return v(tag, msg, null)
+    }
+
+    fun v(tag: String = TAG, tr: Throwable): Int {
+        return v(tag, null, tr)
+    }
+
     fun v(tag: String = TAG, msg: String? = null, tr: Throwable? = null): Int {
         return if (PRIORITY <= VERBOSE) {
             log(VERBOSE, tag, msg, tr)
         } else {
             -1
         }
+    }
+
+    fun d(msg: String): Int {
+        return d(TAG, msg, null)
+    }
+
+    fun d(tr: Throwable): Int {
+        return d(TAG, null, tr)
+    }
+
+    fun d(tag: String = TAG, msg: String): Int {
+        return d(tag, msg, null)
+    }
+
+    fun d(tag: String = TAG, tr: Throwable? = null): Int {
+        return d(tag, null, tr)
     }
 
     fun d(tag: String = TAG, msg: String? = null, tr: Throwable? = null): Int {
@@ -65,12 +97,44 @@ object Log {
         }
     }
 
+    fun i(msg: String): Int {
+        return i(TAG, msg, null)
+    }
+
+    fun i(tr: Throwable): Int {
+        return i(TAG, null, tr)
+    }
+
+    fun i(tag: String = TAG, msg: String): Int {
+        return i(tag, msg, null)
+    }
+
+    fun i(tag: String = TAG, tr: Throwable? = null): Int {
+        return i(tag, null, tr)
+    }
+
     fun i(tag: String = TAG, msg: String? = null, tr: Throwable? = null): Int {
         return if (PRIORITY <= INFO) {
             log(INFO, tag, msg, tr)
         } else {
             -1
         }
+    }
+
+    fun w(tag: String = TAG, msg: String): Int {
+        return w(tag, msg, null)
+    }
+
+    fun w(msg: String): Int {
+        return w(TAG, msg, null)
+    }
+
+    fun w(tr: Throwable): Int {
+        return w(TAG, null, tr)
+    }
+
+    fun w(tag: String = TAG, tr: Throwable? = null): Int {
+        return w(tag, null, tr)
     }
 
     fun w(tag: String = TAG, msg: String? = null, tr: Throwable? = null): Int {
@@ -81,12 +145,44 @@ object Log {
         }
     }
 
+    fun e(msg: String): Int {
+        return e(TAG, msg, null)
+    }
+
+    fun e(tr: Throwable): Int {
+        return e(TAG, null, tr)
+    }
+
+    fun e(tag: String = TAG, msg: String): Int {
+        return e(tag, msg, null)
+    }
+
+    fun e(tag: String = TAG, tr: Throwable? = null): Int {
+        return e(tag, null, tr)
+    }
+
     fun e(tag: String = TAG, msg: String? = null, tr: Throwable? = null): Int {
         return if (PRIORITY <= ERROR) {
             log(ERROR, tag, msg, tr)
         } else {
             -1
         }
+    }
+
+    fun f(msg: String): Int {
+        return f(TAG, msg, null)
+    }
+
+    fun f(tr: Throwable): Int {
+        return f(TAG, null, tr)
+    }
+
+    fun f(tag: String = TAG, msg: String): Int {
+        return f(tag, msg, null)
+    }
+
+    fun f(tag: String = TAG, tr: Throwable? = null): Int {
+        return f(tag, null, tr)
     }
 
     fun f(tag: String = TAG, msg: String? = null, tr: Throwable? = null): Int {
